a8488b8dede86169288a151ef7e6257c43e7c2f2
arewabolu <bolu.arewa@gmail.com>
2022-12-24 13:45:11 +0100

    some simple math funcs

--- firstfile.jl
+++ firstfile.jl
@@ -1,22 +1,27 @@
 println("here's your first file")
 
-#muat assign intervaldefault  value at definition
+# must assign intervaldefault  value at definition
 function countdown(start, stop; interval=1)
     for i in start:interval:stop
         println(i)
     end
 end
 
-function square()
-    # Request a number; print its square
-    print("Enter a whole number: ")
-    num = parse(Float64, readline())  
-    println("\nSquare of $num is $(num*num)")
+function square(array)
+    return [i^2 for i in array] 
 end # square
 
 function mean(arr)
   return  sum(arr)/length(arr)
 end
 
-println(mean([1.0,2.0,3.0]))
-println(countdown(1,5))
+function variance(array)
+    return (sum(square(array))/length(array))-(mean(array)^2)
+end
+
+function stdDev(arr)
+   return sqrt(variance(arr))
+end
+#println(countdown(1,5))
+println(stdDev([1.0,2.0,3.0]))
+
